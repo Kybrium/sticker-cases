1a64f378b7027364fda0b12bfd4cab0379f00eb7
Max <maxchergik@gmail.com>
2025-10-31 17:53:20 +0200

    fix(frontend): use client added for components

--- frontend/src/components/storage/Stickers.tsx
+++ frontend/src/components/storage/Stickers.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import React from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
 
 const Stickers: React.FC<{ data: any }> = ({ data }) => {
@@ -8,10 +11,10 @@ const Stickers: React.FC<{ data: any }> = ({ data }) => {
                     <div className="bg-second-background rounded-2xl relative">
                         <p className="absolute left-1 top-1 px-1 text-white text-xs bg-additional-bg/50 rounded-2xl">Sticker Pack</p>
                         <BsThreeDotsVertical className="absolute right-1 bottom-1 px-1 text-white text-2xl bg-additional-bg/50 rounded-2xl" />
-                        <img src={pack.image_url} />
+                        <img src={pack.image_url} alt={pack.pack_name} />
                     </div>
                     <p className="text-primary-text/80 font-bold">{pack.pack_name}</p>
-                    <p className="text-primary-text/80 text-sm font-extralight flex flex-row gap-2 items-center bg-third-background px-2 py-1 rounded-2xl">{pack.floor_price.toFixed(2)}<img src='https://ton.org/download/ton_symbol.png' className="object-cover w-4 h-4 rounded-full" /></p>
+                    <p className="text-primary-text/80 text-sm font-extralight flex flex-row gap-2 items-center bg-third-background px-2 py-1 rounded-2xl">{pack.floor_price.toFixed(2)}<img alt='TON' src='https://ton.org/download/ton_symbol.png' className="object-cover w-4 h-4 rounded-full" /></p>
                 </div>
             )}
         </div>
